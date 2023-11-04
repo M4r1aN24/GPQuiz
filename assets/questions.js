5d10dfd7bc23e19c1208af1b4fb03c5b9760bb93
M4r1aN24 <142494616+M4r1aN24@users.noreply.github.com>
2023-11-04 13:24:18 +0000

    questions in questions.js it's up to date

---
+++ assets/questions.js
@@ -0,0 +1,44 @@
+var questions = [
+    {
+        question1: "What's the deadliest animal on the planet?",
+        answers1: ["Rhino",
+        "Hippo",
+        "Lion",
+        "Meerkat"],
+        correctAnswerIndex: 1
+    },
+    {
+        question2: "What's my favourite thing to do?",
+        answers2: [
+            "Nothing",
+            "Nothing at all",
+            "Something",
+            "All of the above"
+        ],
+        correctAnswerIndex: 4
+
+    },
+    {
+        question3: "What's my favourite thing to do?",
+        answers3: [
+            "Nothing",
+            "Nothing at all",
+            "Something",
+            "All of the above"
+        ],
+        correctAnswerIndex: 4
+
+    },
+    {
+        question4: "What motorbike do I have?",
+        answers4: [
+            "KTM",
+            "Yamaha",
+            "Suzuki",
+            "Scooter"
+        ],
+        correctAnswerIndex: 1
+
+    }
+    
+];
