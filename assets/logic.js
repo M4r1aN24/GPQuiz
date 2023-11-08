170300e19611aa99ef67ea95ef7b7d69a3c9c60f
M4r1aN24 <142494616+M4r1aN24@users.noreply.github.com>
2023-11-08 22:23:06 +0000

    quiz functionality corrected - all question iteration is working

--- assets/logic.js
+++ assets/logic.js
@@ -3,76 +3,68 @@ var container = document.querySelector("#start-screen");
 var questionsAll = document.querySelector("#questions");
 var answerContainer = document.querySelector("#choices");
 var questionTitle = document.querySelector("#question-title");
-var timeEl = document.querySelector('#time');
+var timeEl = document.querySelector(".time");
 var currentQuestionIndex = 0;
+var secondsLeft = 60;
+var choiceBtn;
+var choiceContainer;
 
 //create an event listener that can iterate over the questions when "start quiz is clicked"
 //if we get type = " button", we do not need event prevent default
-startButton.addEventListener("click",startQuiz);
+startButton.addEventListener("click", startQuiz);
 
-function startQuiz(){
-    displayQuestion();
-    //need the timer here 
+function startQuiz() {
+  displayQuestion();
+  //need the timer here
 
-    // var timerInterval = setInterval(function() {
-    //     secondsLeft--;
-    //     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
-    
-    //     if(secondsLeft === 0) {
-    //       // Stops execution of action at set interval
-    //       clearInterval(timerInterval);
-    //       // Calls function to create and append image
-    //       sendMessage();
-    //     }
-    
-    //   }, 1000);
-    // }
-    
-    // use timer for scoring - when they give a wrong answer, take out of their time and alert them. 
-    // 
-    // collect the points of the answers
-    questionsAll.classList.remove("hide");
-};
+//   var timerInterval = setInterval(function() {
+//       secondsLeft--;
+//       timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
 
-function displayQuestion(){
-    var ul = document.createElement("ul");
+//       if(secondsLeft === 0) {
+//         // Stops execution of action at set interval
+//         clearInterval(timerInterval);
+//         // Calls function to create and append image
+//         sendMessage();
+//       }
 
-    for(var i = 0; i < questions[currentQuestionIndex].answers.length; i++){
-        var li = document.createElement("li");
-        li.textContent = questions[currentQuestionIndex].answers[i];
-        li.addEventListener('click', function(event){
-            // how to target the li as it is but only the text inside the li
-            
-            if(event.target.textContent === questions[currentQuestionIndex].correctAnswerIndex){
-                alert("Correct answer!");
-                console.log(questions[currentQuestionIndex].correctAnswerIndex);
-            } else {
-                alert("wrong answer");
-            }
-            // i need to work out how to change the questions once clicked
-            //check how to take the answer and find out if it's the correct one
-            //if the correct one add 5 seconds,
-            //if the wrong one take 10 seconds.
-            //I still need to change something and move arround, but most of the code is here.
-        })
-        ul.appendChild(li);
-        };
-    answerContainer.appendChild(ul);
-
-    questionTitle.textContent = questions[currentQuestionIndex].question;
-    questionsAll.classList.remove("hide");
+//     }, 1000);
+  
+  container.classList.add("hide");
+  questionsAll.classList.remove("hide");
 }
 
-// when the start quiz button is clicked, the first questions should pop on the screen from questions.js 
+function displayQuestion() {
+choiceContainer = document.createElement("div");
 
-console.log(questions);
+  for (var i = 0; i < questions[currentQuestionIndex].answers.length; i++) {
+    choiceBtn = document.createElement("button");
+    choiceBtn.textContent = questions[currentQuestionIndex].answers[i];
 
-// iterate over the list of questions
-    // if a user clicked on an answer
-    // check the text of the answer (or maybe some data property)
-    // compare that value to the correct answer
-    // if (the thing the user answered is correct)
-    //    display correct!
-    // else
-    //    display incorrect
-    //    subtract time
+    choiceContainer.appendChild(choiceBtn);
+  }
+  answerContainer.appendChild(choiceContainer);
+
+  questionTitle.textContent = questions[currentQuestionIndex].question;
+  questionsAll.classList.remove("hide");
+}
+
+function getQuestion(event) {
+    currentQuestionIndex++;
+    var currentQuestion = questions[currentQuestionIndex];
+    answerContainer.innerHTML = "";
+  
+
+    if (time <= 0 || currentQuestionIndex === questions.length) {
+        quizEnd();
+      } else {
+        displayQuestion();
+      }
+}
+
+function quizEnd(){
+    
+}
+questionTitle.classList.add("hide");
+answerContainer.onclick = getQuestion;
+
